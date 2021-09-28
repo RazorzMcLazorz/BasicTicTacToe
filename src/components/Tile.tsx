3a3cc0329c76f70764ff8988afa6e3f6f871b4bc
RazorzMcLazorz <nateyoungdev@gmail.com>
2021-09-28 09:25:49 -0600

    tic tac toe working

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,14 +1,39 @@
 import * as React from 'react'
 
 interface IProps {
-  tileKey: number[]
+  tileKey: number
+  userTile?: 'X' | 'O'
+  onClick: () => void
 }
 
-interface IState {}
+interface IState {
+  backTileColorShown: boolean
+}
 
 class Tile extends React.Component<IProps, IState> {
+  state: IState = {
+    backTileColorShown: false,
+  }
+
+  onHover = () => {
+    this.setState({ backTileColorShown: true })
+  }
+
+  offHover = () => {
+    this.setState({ backTileColorShown: false })
+  }
+
   render() {
-    return <div className='tile'>{this.props.tileKey.toString()}</div>
+    return (
+      <div
+        className='tile'
+        style={{ backgroundColor: this.state.backTileColorShown ? '#494949' : undefined }}
+        onMouseOver={this.onHover}
+        onMouseOut={this.offHover}
+        onClick={this.props.onClick}>
+        {this.props.userTile ? this.props.userTile : this.props.tileKey}
+      </div>
+    )
   }
 }
 
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,30 +1,50 @@
 import * as React from 'react'
 import Tile from '../components/Tile'
 
+type UserType = 'X' | 'O'
+
+interface UserIndex {
+  [x: number]: UserType
+}
+
 interface IProps {}
 
 interface IState {
   boardSize: number[]
+  currentUser: UserType
+  boardState: Partial<UserIndex>
 }
 
 class App extends React.Component<IProps, IState> {
-  state = {
-    boardSize: [0,1,2],
+  state: IState = {
+    boardSize: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    currentUser: 'X',
+    boardState: {},
   }
+
+  switchUser = (currentUser: UserType) => (currentUser === 'X' ? 'O' : 'X')
+
+  onClick = (index: number, user: UserType) => {
+    this.setState({
+      boardState: { ...this.state.boardState, [index]: user },
+      currentUser: this.switchUser(user),
+    })
+  }
+
   render() {
     return (
-      <div id="boardTable">
-        <div className="board">
-          {this.state.boardSize.map((row: number) => (
-            <>
-              {this.state.boardSize.map((col: number) => {
-                console.log([row, col])
-                return(
-                  <Tile tileKey={[row, col]} />
-                )}
-              )}
-            </>
-          ))}
+      <div id='boardTable'>
+        <div className='currentUser'>Current User is {this.state.currentUser}</div>
+        <div className='board'>
+          {this.state.boardSize.map((index: number) => {
+            return (
+              <Tile
+                tileKey={index}
+                userTile={this.state.boardState[index]}
+                onClick={() => this.onClick(index, this.state.currentUser)}
+              />
+            )
+          })}
         </div>
       </div>
     )
